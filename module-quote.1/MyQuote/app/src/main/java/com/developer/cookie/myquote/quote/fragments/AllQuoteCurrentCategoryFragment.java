fc65fbcb75378bf780db721c41ca5f7d29b8742d
Veselina Zatchepina <techpro20@mail.ru>
2017-05-08 22:54:57 +0300

    refactor: refactoring AllQuoteCurrentCategoryFragment

--- module-quote.1/MyQuote/app/src/main/java/com/developer/cookie/myquote/quote/fragments/AllQuoteCurrentCategoryFragment.java
+++ module-quote.1/MyQuote/app/src/main/java/com/developer/cookie/myquote/quote/fragments/AllQuoteCurrentCategoryFragment.java
@@ -27,6 +27,7 @@ import android.widget.TextView;
 import com.developer.cookie.myquote.R;
 import com.developer.cookie.myquote.database.QuoteDataRepository;
 import com.developer.cookie.myquote.database.model.QuoteText;
+import com.developer.cookie.myquote.quote.Types;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,129 +39,141 @@ import io.realm.RealmResults;
  * AllQuoteCurrentCategoryFragment is used for presentation list of all quotes of current category.
  */
 public class AllQuoteCurrentCategoryFragment extends Fragment {
-
     private static final String LOG_TAG = AllQuoteCurrentCategoryFragment.class.getSimpleName();
-    private static final String QUOTE_TYPE_BUNDLE_AQCCF = "com.developer.cookie.myquote.quote_type_bundle_aqccf";
-    private static final String QUOTE_CATEGORY_BUNDLE_AQCCF = "com.developer.cookie.myquote.quote_category_bundle_aqccf";
-    private static final String QUOTE_SORTED_BUNDLE_AQCCF = "com.developer.cookie.myquote.quote_sorted_bundle_aqccf";
-
-    private static final String QUOTE_CURRENT_CATEGORY_NEW_INSTANCE = "com.developer.cookie.myquote.quote_current_id_new_instance_aqccf";
-    private static final String QUOTE_TYPE_NEW_INSTANCE = "com.developer.cookie.myquote.quote_type_new_instance_aqccf";
-    private static final String QUOTE_SEARCH_BUNDLE_AQCCF = "com.developer.cookie.myquote.quote_search_bundle_aqccf";
-
-    View mRootView;
+    private static final String QUOTE_TYPE_BUNDLE = "all_quote_current_category_fragment_quote_type_bundle";
+    private static final String QUOTE_CATEGORY_BUNDLE = "all_quote_current_category_fragment_quote_category_bundle";
+    private static final String QUOTE_SORTED_BUNDLE = "all_quote_current_category_fragment_quote_sorted_bundle";
+    private static final String QUOTE_CURRENT_CATEGORY_NEW_INSTANCE = "all_quote_current_category_fragment_quote_current_category_new_instance";
+    private static final String QUOTE_TYPE_NEW_INSTANCE = "all_quote_current_category_fragment_quote_type_new_instance";
+    private static final String QUOTE_SEARCH_BUNDLE = "all_quote_current_category_fragment_quote_search_bundle";
+    private AllQuoteCurrentCategoryCallbacks mCallbacks;
     String mCategoryName;
-    QuoteDataRepository mQuoteDataRepository;
-    ArrayList<Long> mListOfQuotesId;
-    List<String> mCurrentCategoryQuoteTextList;
-
-    AllQuoteCurrentCategoryRecyclerViewAdapter mRecyclerViewAdapter;
-
-    RealmResults<QuoteText> mQuoteTexts;
-
     String mQuoteType;
+    QuoteDataRepository mQuoteDataRepository;
+    RealmResults<QuoteText> mQuoteTexts;
+    ArrayList<Long> mListOfQuotesId;
+    List<String> mCurrentCategoryQuoteTexts;
+    RecyclerView mRecyclerView;
+    AllQuoteCurrentCategoryRecyclerViewAdapter mRecyclerViewAdapter;
     String mSortedBy;
     long mCurrentId;
-
-    RecyclerView mRecyclerView;
-
-    private AllQuoteCurrentCategoryCallbacks mCallbacks;
-
     SearchView mSearchView;
     String mSearchText;
 
-    public AllQuoteCurrentCategoryFragment() {
-        super();
-    }
+    public AllQuoteCurrentCategoryFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        defineInputData(savedInstanceState);
+        mQuoteDataRepository = new QuoteDataRepository();
+        mQuoteTexts = mQuoteDataRepository.getListOfQuotesByCategory(mCategoryName, mQuoteType);
+    }
+
+    private void defineInputData(Bundle savedInstanceState) {
         if (savedInstanceState != null) {
             setDataFromSavedInstanceState(savedInstanceState);
         } else if (getArguments() != null) {
             mCategoryName = getArguments().getString(QUOTE_CURRENT_CATEGORY_NEW_INSTANCE);
             mQuoteType = getArguments().getString(QUOTE_TYPE_NEW_INSTANCE);
         }
-        mQuoteDataRepository = new QuoteDataRepository();
-        mQuoteTexts = mQuoteDataRepository.getListOfQuotesByCategory(mCategoryName, mQuoteType);
+    }
+
+    private void setDataFromSavedInstanceState(Bundle savedInstanceState) {
+        mQuoteType = savedInstanceState.getString(QUOTE_TYPE_BUNDLE);
+        mCategoryName = savedInstanceState.getString(QUOTE_CATEGORY_BUNDLE);
+        mSortedBy = savedInstanceState.getString(QUOTE_SORTED_BUNDLE);
+        mSearchText = savedInstanceState.getString(QUOTE_SEARCH_BUNDLE);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mRootView = inflater.inflate(R.layout.fragment_recyclerview, container, false);
-
-        TextView currentCategory = (TextView) mRootView.findViewById(R.id.current_category);
-        currentCategory.setText(mCategoryName);
-        currentCategory.setAllCaps(true);
-
+        final View rootView = inflater.inflate(R.layout.fragment_recyclerview, container, false);
+        setCurrentCategoryTitle(rootView);
         mQuoteTexts.addChangeListener(new RealmChangeListener<RealmResults<QuoteText>>() {
             @Override
             public void onChange(RealmResults<QuoteText> element) {
                 if (element.size() == 0 && getActivity() != null) {
                     getActivity().finish();
                 }
-                mCurrentCategoryQuoteTextList = getListOfQuoteText(element);
-                mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.recycler_view);
-                registerForContextMenu(mRecyclerView);
-                mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-                mRecyclerViewAdapter = new AllQuoteCurrentCategoryRecyclerViewAdapter(mCurrentCategoryQuoteTextList);
-                mRecyclerView.setAdapter(mRecyclerViewAdapter);
-                // Create list of quote's id for data transfer to another fragment
-                mListOfQuotesId = new ArrayList<Long>();
-                for (int i = 0; i < element.size(); i++) {
-                    mListOfQuotesId.add(element.get(i).getId());
-                }
-                if (isAdded()) {
-                    if (getActivity().findViewById(R.id.detail_fragment_container) != null && !element.isEmpty()) {
-                        mCallbacks.onQuoteSelected(mListOfQuotesId, element.first().getId());
-                    }
-                }
+                getListOfQuoteTexts(element);
+                getListOfIds(element);
+                defineRecyclerView(rootView);
+                setFirstCurrentQuoteForTablet(element);
                 if (mSortedBy != null) {
                     sortAndUpdateRecyclerView(mSortedBy);
                 }
             }
         });
 
-        return mRootView;
+        return rootView;
     }
 
-    private void setDataFromSavedInstanceState(Bundle savedInstanceState) {
-        mQuoteType = savedInstanceState.getString(QUOTE_TYPE_BUNDLE_AQCCF);
-        mCategoryName = savedInstanceState.getString(QUOTE_CATEGORY_BUNDLE_AQCCF);
-        mSortedBy = savedInstanceState.getString(QUOTE_SORTED_BUNDLE_AQCCF);
-        mSearchText = savedInstanceState.getString(QUOTE_SEARCH_BUNDLE_AQCCF);
+    private void setCurrentCategoryTitle(View rootView) {
+        TextView currentCategoryTitle = (TextView) rootView.findViewById(R.id.current_category);
+        currentCategoryTitle.setText(mCategoryName);
+        currentCategoryTitle.setAllCaps(true);
+    }
+
+    private void getListOfQuoteTexts(RealmResults<QuoteText> realmResults) {
+        mCurrentCategoryQuoteTexts = new ArrayList<String>();
+        for (QuoteText quoteText : realmResults) {
+            mCurrentCategoryQuoteTexts.add(quoteText.getQuoteText());
+        }
+    }
+
+    private void defineRecyclerView(View rootView) {
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerViewAdapter = new AllQuoteCurrentCategoryRecyclerViewAdapter(mCurrentCategoryQuoteTexts);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+    }
+
+    private void setFirstCurrentQuoteForTablet(RealmResults<QuoteText> realmResults) {
+        if (isAdded()) {
+            if (getActivity().findViewById(R.id.detail_fragment_container) != null && !realmResults.isEmpty()) {
+                mCallbacks.onQuoteSelected(mListOfQuotesId, realmResults.first().getId());
+            }
+        }
+    }
+
+    private void getListOfIds(RealmResults<QuoteText> realmResults) {
+        mListOfQuotesId = new ArrayList<Long>();
+        for (QuoteText quoteText : realmResults) {
+            mListOfQuotesId.add(quoteText.getId());
+        }
+    }
+
+    private void sortAndUpdateRecyclerView(String mSortedBy) {
+        mQuoteTexts = mQuoteTexts.sort(mSortedBy);
+        getListOfQuoteTexts(mQuoteTexts);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_current_all_quotes, menu);
-        if (mQuoteType.equals("My quote")) {
+        defineVisibilityFilterMenu(menu);
+        defineSearchView(menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void defineVisibilityFilterMenu(Menu menu) {
+        if (mQuoteType.equals(Types.MY_QUOTE)) {
             menu.findItem(R.id.filter_quote).setVisible(false);
         }
+    }
+
+    private void defineSearchView(Menu menu) {
         MenuItem search = menu.findItem(R.id.search_quote);
         mSearchView = (SearchView) MenuItemCompat.getActionView(search);
         search(mSearchView);
-        //focus the SearchView
         if (mSearchText != null && !mSearchText.isEmpty()) {
             search.expandActionView();
             mSearchView.setQuery(mSearchText, true);
             mSearchView.clearFocus();
         }
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.filter_quote:
-                ActionMenuItemView view = (ActionMenuItemView) getActivity().findViewById(R.id.filter_quote);
-                showPopup(view);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void search(SearchView searchView) {
@@ -176,6 +189,17 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filter_quote:
+                ActionMenuItemView view = (ActionMenuItemView) getActivity().findViewById(R.id.filter_quote);
+                showPopup(view);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void showPopup(View v) {
@@ -206,20 +230,6 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
         popup.show();
     }
 
-    private void sortAndUpdateRecyclerView(String mSortedBy) {
-        mQuoteTexts = mQuoteTexts.sort(mSortedBy);
-        List<String> quoteTextList = getListOfQuoteText(mQuoteTexts);
-        mRecyclerViewAdapter.changeDate(quoteTextList);
-    }
-
-    private List<String> getListOfQuoteText(RealmResults<QuoteText> realmResults) {
-        List<String> quoteTextList = new ArrayList<String>();
-        for (int i = 0; i < realmResults.size(); i++) {
-            quoteTextList.add(realmResults.get(i).getQuoteText());
-        }
-        return quoteTextList;
-    }
-
     public static AllQuoteCurrentCategoryFragment newInstance(String quoteCategory, String quoteType) {
         Bundle args = new Bundle();
         args.putString(QUOTE_CURRENT_CATEGORY_NEW_INSTANCE, quoteCategory);
@@ -232,11 +242,11 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(QUOTE_TYPE_BUNDLE_AQCCF, mQuoteType);
-        outState.putString(QUOTE_CATEGORY_BUNDLE_AQCCF, mCategoryName);
-        outState.putString(QUOTE_SORTED_BUNDLE_AQCCF, mSortedBy);
+        outState.putString(QUOTE_TYPE_BUNDLE, mQuoteType);
+        outState.putString(QUOTE_CATEGORY_BUNDLE, mCategoryName);
+        outState.putString(QUOTE_SORTED_BUNDLE, mSortedBy);
         mSearchText = mSearchView.getQuery().toString();
-        outState.putString(QUOTE_SEARCH_BUNDLE_AQCCF, mSearchText);
+        outState.putString(QUOTE_SEARCH_BUNDLE, mSearchText);
 
     }
 
@@ -268,6 +278,11 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
         private List<String> currentCategoryQuoteList;
         private List<String> filteredQuoteTextList;
 
+        public AllQuoteCurrentCategoryRecyclerViewAdapter(List<String> listOfQuoteText) {
+            currentCategoryQuoteList = listOfQuoteText;
+            filteredQuoteTextList = listOfQuoteText;
+        }
+
         class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
             TextView currentQuote;
 
@@ -280,21 +295,20 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
 
             @Override
             public void onClick(View view) {
-                mCurrentId = mListOfQuotesId.get(mCurrentCategoryQuoteTextList
+                mCurrentId = mListOfQuotesId.get(mCurrentCategoryQuoteTexts
                         .indexOf(currentQuote.getText().toString()));
                 mCallbacks.onQuoteSelected(mListOfQuotesId, mCurrentId);
             }
 
             @Override
             public boolean onLongClick(View view) {
-                mCurrentId = mListOfQuotesId.get(mCurrentCategoryQuoteTextList
+                mCurrentId = mListOfQuotesId.get(mCurrentCategoryQuoteTexts
                         .indexOf(currentQuote.getText().toString()));
                 openDeleteQuoteDialog();
                 return false;
             }
         }
 
-        //For searchView
         @Override
         public Filter getFilter() {
             return new Filter() {
@@ -326,7 +340,6 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
         }
 
         private void openDeleteQuoteDialog() {
-            // Create dialog for delete current quote
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View dialogView = layoutInflater.inflate(R.layout.dialog_delete, null);
             TextView deleteDialogTitle = (TextView) dialogView.findViewById(R.id.dialog_delete_title);
@@ -339,9 +352,7 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
                                     mQuoteDataRepository.deleteQuoteByIdFromDb(mCurrentId, mQuoteType);
-                                    final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) getActivity().findViewById(R.id.coordinator_layout);
-                                    Snackbar snackbarIsDeleted = Snackbar.make(coordinatorLayout, "Quote is deleted!", Snackbar.LENGTH_LONG);
-                                    snackbarIsDeleted.show();
+                                    showSnackbar();
                                 }
                             })
                     .setNegativeButton(getResources().getString(R.string.dialog_cancel_button),
@@ -354,14 +365,10 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
             alertDialog.show();
         }
 
-        /**
-         * Method create custom adapter.
-         *
-         * @param listOfQuoteText
-         */
-        public AllQuoteCurrentCategoryRecyclerViewAdapter(List<String> listOfQuoteText) {
-            currentCategoryQuoteList = listOfQuoteText;
-            filteredQuoteTextList = listOfQuoteText;
+        private void showSnackbar() {
+            final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) getActivity().findViewById(R.id.coordinator_layout);
+            Snackbar snackbarIsDeleted = Snackbar.make(coordinatorLayout, "Quote is deleted!", Snackbar.LENGTH_LONG);
+            snackbarIsDeleted.show();
         }
 
         @Override
@@ -379,16 +386,6 @@ public class AllQuoteCurrentCategoryFragment extends Fragment {
         @Override
         public int getItemCount() {
             return filteredQuoteTextList.size();
-        }
-
-        /**
-         * When adapter data is changed this method helps set new data for adapter.
-         *
-         * @param listOfQuoteText
-         */
-        public void changeDate(List<String> listOfQuoteText) {
-            filteredQuoteTextList = listOfQuoteText;
-            notifyDataSetChanged();
         }
     }
 
