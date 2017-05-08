2b4f3f32d6f304c7e11e9da2169be6059ec6a1b7
Veselina Zatchepina <techpro20@mail.ru>
2017-05-09 00:28:00 +0300

    refactor: refactoring FillViewsWithCurrentQuoteDataHelper

--- module-quote.1/MyQuote/app/src/main/java/com/developer/cookie/myquote/utils/FillViewsWithCurrentQuoteDataHelper.java
+++ module-quote.1/MyQuote/app/src/main/java/com/developer/cookie/myquote/utils/FillViewsWithCurrentQuoteDataHelper.java
@@ -1,12 +1,12 @@
 package com.developer.cookie.myquote.utils;
 
 
-import android.view.View;
 import android.widget.TextView;
 
 import com.developer.cookie.myquote.database.model.BookAuthor;
 import com.developer.cookie.myquote.database.model.BookName;
 import com.developer.cookie.myquote.database.model.QuoteText;
+import com.developer.cookie.myquote.quote.Types;
 
 import io.realm.RealmList;
 import io.realm.RealmResults;
@@ -17,41 +17,61 @@ import io.realm.RealmResults;
  */
 public class FillViewsWithCurrentQuoteDataHelper {
 
+    private static BookName bookName;
+
     public static void fillViewsWithCurrentQuoteData(RealmResults<QuoteText> realmResults,
                                                      TextView quoteTextView, TextView bookNameView,
                                                      TextView authorNameView, TextView pageNumberView,
                                                      TextView publisherNameTextView,
                                                      TextView yearNumberView,
                                                      String quoteType) {
-        QuoteText quoteTextObject = realmResults.first();
-        quoteTextView.setText(quoteTextObject.getQuoteText());
-        if (quoteType.equals("Book quote")) {
-            BookName bookName = quoteTextObject.getBookName();
-            bookNameView.setText(bookName.getBookNameValue());
-            RealmList<BookAuthor> bookAuthors = bookName.getBookAuthors();
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < bookAuthors.size(); i++) {
-                if (i != bookAuthors.size() - 1) {
-                    builder.append(bookAuthors.get(i).getBookAuthorName()).append(", ");
-                } else {
-                    builder.append(bookAuthors.get(i).getBookAuthorName());
-                }
-            }
-            String authorName = builder.toString();
-            authorNameView.setText(authorName);
-            String bookPage = quoteTextObject.getPage().getPageNumber();
-            pageNumberView.setText(bookPage);
-            String bookPublisher = bookName.getPublisher().getPublisherName();
-            publisherNameTextView.setText(bookPublisher);
-            String bookPublicationYear = bookName.getYear().getYearNumber();
-            yearNumberView.setText(bookPublicationYear);
-
-        } else {
-            bookNameView.setVisibility(View.GONE);
-            authorNameView.setVisibility(View.GONE);
-            pageNumberView.setVisibility(View.GONE);
-            yearNumberView.setVisibility(View.GONE);
-            publisherNameTextView.setVisibility(View.GONE);
+        QuoteText quote = realmResults.first();
+        fillQuoteTextField(quoteTextView, quote);
+        if (quoteType.equals(Types.BOOK_QUOTE)) {
+            bookName = fillBookNameField(bookNameView, quote);
+            fillBookAuthorField(authorNameView, bookName);
+            fillBookPageField(pageNumberView, quote);
+            fillBookPublisherField(publisherNameTextView, bookName);
+            fillBookPublicationYearField(yearNumberView, bookName);
         }
+    }
+
+    private static void fillQuoteTextField(TextView quoteTextView, QuoteText quote) {
+        quoteTextView.setText(quote.getQuoteText());
+    }
+
+    private static BookName fillBookNameField(TextView bookNameView, QuoteText quote) {
+        BookName bookName = quote.getBookName();
+        bookNameView.setText(bookName.getBookNameValue());
+        return bookName;
+    }
+
+    private static void fillBookAuthorField(TextView authorNameView, BookName bookName) {
+        RealmList<BookAuthor> bookAuthors = bookName.getBookAuthors();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < bookAuthors.size(); i++) {
+            if (i != bookAuthors.size() - 1) {
+                builder.append(bookAuthors.get(i).getBookAuthorName()).append(", ");
+            } else {
+                builder.append(bookAuthors.get(i).getBookAuthorName());
+            }
+        }
+        String authorName = builder.toString();
+        authorNameView.setText(authorName);
+    }
+
+    private static void fillBookPageField(TextView pageNumberView, QuoteText quote) {
+        String bookPage = quote.getPage().getPageNumber();
+        pageNumberView.setText(bookPage);
+    }
+
+    private static void fillBookPublisherField(TextView publisherNameTextView, BookName bookName) {
+        String bookPublisher = bookName.getPublisher().getPublisherName();
+        publisherNameTextView.setText(bookPublisher);
+    }
+
+    private static void fillBookPublicationYearField(TextView yearNumberView, BookName bookName) {
+        String bookPublicationYear = bookName.getYear().getYearNumber();
+        yearNumberView.setText(bookPublicationYear);
     }
 }
